d26b89da01f4b7248081aab9b2a2254295222a51
devgenx <sebastian110420@gmail.com>
2022-12-13 17:28:01 -0500

    commit tree f4070a83e533728060c7298ce49000b7fa977f84

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    return "This one is on me!";
+  } else if (ride > 2000 && ride <= 2499) {
+    return "I will gladly take your thirty bucks.";
+  } else if (ride > 2500) {
+    return "No can do.";
+  } else {
+    return "That will be twenty bucks.";
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+// function ternaryCheckCity(city) {
+//   // Write your code here!
+//   if (city === "NYC") {
+//     return "Ok, sounds good.";
+//   } else {
+//     return "No go.";
+//   }
+// }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+// function ternaryCheckCity(city) {
+//   // Write your code here!
+//   return city === "NYC" ? "Ok, sounds good." : "No go.";
+// }
+
+const ternaryCheckCity = (city) =>
+  city === "NYC" ? "Ok, sounds good." : "No go.";
+
+const switchOnCharmFromTip = (tip) => {
+  switch (tip) {
+    case "generous":
+    case "GENEROUS":
+      return "Thank you so much.";
+      break;
+
+    case "not as generous":
+    case "NOT AS GENEROUS":
+      return "Thank you.";
+      break;
+
+    default:
+      return "Bye.";
+  }
+};
